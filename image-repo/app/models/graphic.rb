d96aae8e9ec7450fe8b8b6d1684d251aa9123806
Sam O <sam.orend22@gmail.com>
2021-05-05 21:05:03 -0400

    Unit tests for model

--- image-repo/app/models/graphic.rb
+++ image-repo/app/models/graphic.rb
@@ -1,3 +1,6 @@
 class Graphic < ApplicationRecord
     has_one_attached :image, :dependent => :destroy
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :image, presence: true, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 end
--- image-repo/test/models/graphic_test.rb
+++ image-repo/test/models/graphic_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class GraphicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save graphic without title" do
+    graphic = Graphic.new
+    assert_not graphic.save, "Saved the graphic without a title"
+  end
+  test "should not save graphic without description" do
+    graphic = Graphic.new(:title => "Example Title")
+    assert_not graphic.save, "Saved the graphic without a description"
+  end
+  test "should not save graphic without image" do
+    graphic = Graphic.new(:title => "Example Title", :description => "This is an image of a thing")
+    assert_not graphic.save, "Saved the graphic without a description"
+  end
+  test "should save graphic with title, description and png" do
+    graphic = Graphic.new(:title => "Example Title", :description => "This is an image of a thing")
+    path = File.join Rails.root, 'test', 'models'
+    graphic.image.attach(io: File.open(File.join(path, 'logo512.png')), filename: 'logo512.png')
+    assert graphic.save, "Failed to save normal graphic"
+  end
+  test "should save graphic with title, description and jpeg" do
+    graphic = Graphic.new(:title => "Example Title", :description => "This is an image of a thing")
+    path = File.join Rails.root, 'test', 'models'
+    graphic.image.attach(io: File.open(File.join(path, 'demo.jpeg')), filename: 'demo.jpeg')
+    assert graphic.save, "Failed to save normal graphic"
+  end
+  test "should not save graphic with non-image formatted file" do
+    graphic = Graphic.new(:title => "Example Title", :description => "This is an image of a thing")
+    path = File.join Rails.root, 'test', 'models'
+    graphic.image.attach(io: File.open(File.join(path, 'ex.pdf')), filename: 'ex.pdf')
+    assert_not graphic.save, "Saved graphic with wrong file format"
+  end
 end
